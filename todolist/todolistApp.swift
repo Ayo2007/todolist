fc09f32c3567bc504f84134c7450cbe214e0876d
Ayo <aogunkinle@lcs.on.ca>
2024-04-08 13:49:39 -0400

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Ayodeji Ogunkinle on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
