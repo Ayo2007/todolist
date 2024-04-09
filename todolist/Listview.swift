68a8abcd44e8ccd4799a3f7f8e6a1002073f7a9b
Ayo <aogunkinle@lcs.on.ca>
2024-04-09 14:32:22 -0400

    finished the interface moving on to part 2

---
+++ todolist/Listview.swift
@@ -0,0 +1,18 @@
+//
+//  Listview.swift
+//  todolist
+//
+//  Created by Ayodeji Ogunkinle on 2024-04-08.
+//
+
+import SwiftUI
+
+struct Listview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Listview()
+}
