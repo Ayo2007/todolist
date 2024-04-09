f6a7996c8d895f7fc2de0092383ef9464c87f3f8
Ayo <aogunkinle@lcs.on.ca>
2024-04-09 14:32:52 -0400

    Finished the interface moving on to part 2

--- todolist/ContentView.swift
+++ todolist/ContentView.swift
@@ -6,14 +6,42 @@
 //
 
 import SwiftUI
+// MARK: Stored Properties
+
+
+
 
 struct ContentView: View {
+    
+    //The item currently being added
+    @State var newItemDiscrpit = ""
+    
+    
+    // The search text
+    @State var searchText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack{
+                List{
+                    
+                   
+                    ItemView(title: "Study for Chemistry quiz", done: false)
+                    ItemView(title: "Finish Computer Scienece assignment", done: true)
+                    ItemView(title: "Go for a run around campus", done: false)
+                }
+                .searchable(text: $searchText)
+                HStack{
+                    TextField("Enter a to-do item", text: $newItemDiscrpit)
+                    Button("ADD") {
+                        // Add the new item to to-do
+                    }
+                    .font(.caption)
+                    
+                }
+                .padding(20)
+            }
+            .navigationTitle("To Do")
         }
         .padding()
     }
@@ -21,4 +49,17 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ItemView: View {
+    
+    let title: String
+    let done: Bool
+    
+    var body: some View {
+        Label(
+            title: { Text(title) },
+            icon: { Image(systemName: done == true ? "checkmark.circle" : "circle") }
+        )
+    }
 }
--- todolist/Listview.swift
+++ todolist/Listview.swift
@@ -4,15 +4,37 @@
 //
 //  Created by Ayodeji Ogunkinle on 2024-04-08.
 //
-
 import SwiftUI
 
-struct Listview: View {
+struct ListView: View {
+    
+    //MARK: Stored properties
+    let reminder: String
+    let reminderColor: Color
+   
+    
+    //MARK: Computed properties
+    
+    // Returns the button's user interface...
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+       
+        HStack {
+            
+            Image(systemName: "circle")
+                .foregroundColor(Color.blue)
+            
+            VStack (alignment: .leading) {
+                
+                // First layer
+                Text(reminder)
+                    .foregroundStyle(reminderColor)
+              
+            }
+        }
     }
 }
 
 #Preview {
-    Listview()
+    ListView(reminder:"Call auto body shop", reminderColor: .black)
 }
